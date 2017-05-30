a874a43189f88a4f8602d921b883692d78c16160
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-05-30 13:31:52 -0300

    Pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -20,4 +20,23 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size) 
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)    
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)  
+  end
+
 end
