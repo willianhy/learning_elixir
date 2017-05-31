da6f5952aec96f9b379b178e796b28614d7eb8bf
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-05-31 13:31:22 -0300

    Adding documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,14 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a list of string representing a deck playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
-    suits = ["Spaces", "Clubs", "Hearts", "Diamonds"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
@@ -13,10 +19,30 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+  ##Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The 'hand_size' argument indicates how many cards should be in the hand.
+
+  ##Examples  
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size) 
   end
@@ -34,9 +60,8 @@ defmodule Cards do
   end
 
   def create_hand(hand_size) do
-    deck = Cards.create_deck
-    deck = Cards.shuffle(deck)
-    hand = Cards.deal(deck, hand_size)  
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
-
 end
