c948fbe47896a79dca4f2ccdd50b65e2d57b9d57
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-06-05 13:32:43 -0300

    Building the grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,9 +3,35 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
   end
 
-  def pick_color(%Identicon.Image{ hex: [r, g, b | _tail]} = image) do    
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0 
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex } = image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do    
     %Identicon.Image{image | color: {r, g, b}}
   end
 
