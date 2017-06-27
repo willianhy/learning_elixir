b5c5a1c35576d8899db5d2667625270008123829
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-06-27 13:26:07 -0300

    topic controller - saving records

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -8,6 +8,11 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do    
+    changeset = Topic.changeset(%Topic{}, topic)
 
+    case Repo.insert(changeset) do      
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end    
 end
