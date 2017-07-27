e387699f250f223b0401d8c5f6002b70c1532b1a
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-07-27 13:33:45 -0300

    configuring oauth

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: "6e5ad7c467310d3be9f0",
+  client_secret: "0f044be74b15d6f5d1ec01da8458a4e54b9d9f17"
